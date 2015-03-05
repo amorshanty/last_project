99b77e72a9f4c4a65b49e841e4d77e621b6be652
Amor <amor.dibella@gmail.com>
2015-03-05 17:27:38 +0100

    fix url picture

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,38 @@
 require "faker"
 
 Category.create(
+	name: "Where to live" ,
+	summary: "Recommendations for cool areas for live",
+	image_url: "http://blog.lifestylebarcelona.com/wp-content/uploads/2013/08/gracia-barcelona-2011.jpg"
+	)
+
+Category.create(
+	name: "Where to go" ,
+	summary: "Nice places to visit those days you want to see new things",
+	image_url: "http://www.the-interiordesign.com/progprodimg/IMG_1310.jpg"
+	)
+
+Category.create(
 	name: "Food" ,
 	summary: "Best places to eat the best food in Barna",
 	image_url: "http://www.apartmentbarcelona.com/blog/wp-content/uploads/2012/07/Princesa-23-Barceloneta.jpg"
 	)
 
+
 Category.create(
-	name: "Shoping" ,
+	name: "Telephone line" ,
+	summary: "Best tips for new users of phone lines here in Barna",
+	image_url: "http://www.vsacomunicacion.com/blog/wp-content/uploads/2013/07/Vodafone-Gran-Via-13-Madrid.jpg"
+	)
+
+Category.create(
+	name: "Shopping" ,
 	summary: "Best stores without spending much money",
 	image_url: "https://bellowblogs.files.wordpress.com/2012/02/la-boqueria-68.jpg"
 	)
 
 Category.create(
-	name: "Hostal" ,
+	name: "Hostel" ,
 	summary: "Best places you can get to spend that first night",
 	image_url: "http://3.bp.blogspot.com/-Va0FdeCJdzY/TmtP0GIIaBI/AAAAAAAAJaU/jNqhV2Ap-t8/s1600/BARCELONA_PASSEIG_DE_GRACIA_PLA%25C3%2587A_JOAN_CARLES.jpg"
 	)
@@ -38,7 +57,7 @@ Category.create(
 	)
 
 Category.create(
-	name: "Partyng" ,
+	name: "Partying" ,
 	summary: "The best places to party, categorized by musical genres",
 	image_url: "http://cdnstatic-8.mydestination.com/library/images/583485_1000_667.jpg"
 	)
@@ -53,24 +72,6 @@ Category.create(
 	name: "Jobs" ,
 	summary: "Best tips if you're looking for a new job",
 	image_url: "http://2.bp.blogspot.com/_LFsy49yCUjg/TKf42LhexKI/AAAAAAAAAkY/Abe3EjTWpsw/s1600/Barcelona_subway_D3S7567.jpg"
-	)
-
-Category.create(
-	name: "Telephone line" ,
-	summary: "Best tips for new users of phone lines here in Barna",
-	image_url: "http://www.vsacomunicacion.com/blog/wp-content/uploads/2013/07/Vodafone-Gran-Via-13-Madrid.jpg"
-	)
-
-Category.create(
-	name: "Where to live" ,
-	summary: "Recommendations for cool areas for live",
-	image_url: "http://blog.lifestylebarcelona.com/wp-content/uploads/2013/08/gracia-barcelona-2011.jpg"
-	)
-
-Category.create(
-	name: "Where to go" ,
-	summary: "Nice places to visit those days you want to see new things",
-	image_url: "http://www.the-interiordesign.com/progprodimg/IMG_1310.jpg"
 	)
 
 Category.create(
